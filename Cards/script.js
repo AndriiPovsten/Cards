e3249381c350cb7534282a8d8f601d6d75368164
Andrew Povsten <80416008+Povsten228322@users.noreply.github.com>
2022-07-11 12:15:54 +0200

    Add files via upload

---
+++ Cards/script.js
@@ -0,0 +1,12 @@
+let slides  = document.querySelectorAll('.slide');
+for (const slide of slides) {
+    slide.addEventListener('click',() => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+}
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
